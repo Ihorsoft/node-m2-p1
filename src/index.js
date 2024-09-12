e090683b7f5d725b63cc911cd54ed0e715fefe03
Ihorsoft <0639492002s@gmail.com>
2024-09-12 14:31:42 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log(process.cwd());
-console.log(process.argv);
+//src/index.js
+import { startServer } from './server.js';
+import { initMongoDB } from './db/initMongoDB.js';
+
+const bootstrap = async () => {
+  await initMongoDB();
+  console.log('before start server in index.js');
+  startServer();
+  console.log('after  start server in index.js');
+};
+bootstrap();
